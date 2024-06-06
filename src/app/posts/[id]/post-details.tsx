d2e508d9b2f01082fd5ccf40b5ba0a3eccb92171
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-06-06 16:21:31 -0500

    take out image file input

--- src/app/posts/[id]/post-details.tsx
+++ src/app/posts/[id]/post-details.tsx
@@ -1,5 +1,4 @@
 import prisma from "@/lib/db";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export default async function PostDetails({ params }: { params: { id: string } }) {
@@ -16,11 +15,6 @@ export default async function PostDetails({ params }: { params: { id: string } }
     <main className="text-center pt-28 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">{post.title}</h1>
       <p className="max-w-[700px] mx-auto leading-8 border border-neutral-700">{post.body}</p>
-      {post.imageUrl && (
-        <div className="mx-auto mb-5 max-w-[700px]">
-          <Image src={post.imageUrl} alt="Post Image" width={700} height={475} />
-        </div>
-      )}
     </main>
   );
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,54 +1,17 @@
-"use client"
 import { createPost } from "@/actions/actions";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Form() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [imageUrl, setImageUrl] = useState<string | null>("");
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      if (typeof reader.result === "string") {
-        setImageUrl(reader.result);
-      }
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // Convert the image data to base64
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("body", body);
-    if (imageUrl) {
-      formData.append("imageUrl", imageUrl);
-    }
-
-    await createPost(formData);
-  };
-
   return (
     <form
       action={createPost}
       className="flex flex-col max-w-[400px] mx-auto gap-2 my-10 text-stone-400"
-      onSubmit={handleSubmit}
     >
       <input
         type="text"
         name="title"
         placeholder="Title for new post"
         className="border rounded px-3 h-10 text-stone-600"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
         required
       />
       <textarea
@@ -56,16 +19,14 @@ export default function Form() {
         placeholder="Body content for new post"
         className="border rounded px-3 py-2 text-stone-600"
         rows={6}
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
         required
       />
       <input
-        type="file"
+        type="string"
         name="imageUrl"
+        placeholder="Image URL for new post"
         accept="image/*"
         className="border rounded px-3 h-10 text-stone-600"
-        onChange={handleFileChange}
       />
       <button className="h-10 bg-blue-500 px-5 rounded text-white">
         Submit
