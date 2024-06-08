d15d90f96b97387d8f62ddf8a20d19adb6a78e1d
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-06-07 20:14:12 -0500

    remove upload file, try diff approach tmrw

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -4,7 +4,6 @@ import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import fs from "node:fs/promises";
 
 export async function createPost(formData: FormData) {
   const { isAuthenticated } = getKindeServerSession();
@@ -15,32 +14,15 @@ export async function createPost(formData: FormData) {
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
   const imageUrl = formData.get("imageUrl") as string;
-  const file = formData.get("file") as File;
-
-  let image = null;
-  if (file) {
-    const arrayBuffer = await file.arrayBuffer();
-    image = Buffer.from(arrayBuffer);
-  }
+  console.log(title, body);
   
   await prisma.post.create({
     data: {
       title,
       body,
       imageUrl,
-      image,
     },
   });
 
   revalidatePath("/posts");
-}
-
-export async function uploadFile(formData: FormData) {
-  const file = formData.get("file") as File;
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = new Uint8Array(arrayBuffer);
-
-  await fs.writeFile(`./public/uploads/${file.name}`, buffer);
-
-  revalidatePath("/");
 }
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,85 +1,8 @@
-// import { Suspense } from "react";
-// import prisma from "@/lib/db";
-// import { notFound } from "next/navigation";
-// import UploadForm from "@/components/upload-form";
-// import Image from "next/image";
-// import fs from "node:fs/promises"; 
-
-// export default async function Page({ params }: { params: { id: string } }) {
-//   const post = await prisma.post.findUnique({
-//     where: {
-//       id: parseInt(params.id),
-//     },
-//   });
-//   if (!post) {
-//     notFound();
-//   }
-
-//    // Ensure the public/uploads directory exists
-//   const publicDir = "./public";
-//   const uploadsDir = "./public/uploads";
-//   try {
-//     await fs.access(publicDir);
-//   } catch (error) {
-//     await fs.mkdir(publicDir);
-//   }
-
-//   try {
-//     await fs.access(uploadsDir);
-//   } catch (error) {
-//     await fs.mkdir(uploadsDir);
-//   }
-
-//   const files = await fs.readdir("./public/uploads");
-//   const images = files
-//     .filter((file) => file.endsWith(".jpg"))
-//     .map((file) => `/uploads/${file}`);
-  
-//   return (
-//     <main className="text-center pt-16 px-5">
-//       <Suspense fallback="Loading...">
-//           <h1 className="text-4xl md:text-5xl font-bold mb-5">{post.title}</h1>
-//           <p className="max-w-[700px] mx-auto leading-8 border border-neutral-700">{post.body}</p>
-        
-//         <div className="flex flex-wrap justify-center mt-3">
-//           {images.map((image) => (
-//             <div key={image} className="px-2 h-auto w-2/3">
-//               <Image
-//                 key={image}
-//                 src={image}
-//                 width={600}
-//                 height={600}
-//                 alt={image}
-//                 className="object-cover w-full"
-//               />
-//             </div>
-//           ))}
-//         </div>
-//         <UploadForm />
-//       </Suspense>
-//     </main>
-//   )
-// }
 import { Suspense } from "react";
 import prisma from "@/lib/db";
 import { notFound } from "next/navigation";
-import UploadForm from "@/components/upload-form";
-import Image from "next/image";
 
-// This function converts the image buffer to a base64 string
-const bufferToBase64 = (buffer: ArrayBuffer) => {
-  return Buffer.from(buffer).toString('base64');
-}
-
-interface Params {
-  id: string;
-}
-
-interface PageProps {
-  params: Params;
-}
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: { params: { id: string } }) {
   const post = await prisma.post.findUnique({
     where: {
       id: parseInt(params.id),
@@ -88,23 +11,15 @@ export default async function Page({ params }: PageProps) {
   if (!post) {
     notFound();
   }
-
+  
   return (
     <main className="px-7 pt-24 text-center">
       <Suspense fallback="Loading...">
-        <div className="text-center pt-28 px-5">
+        <main className="text-center pt-28 px-5">
           <h1 className="text-4xl md:text-5xl font-bold mb-5">{post.title}</h1>
           <p className="max-w-[700px] mx-auto leading-8 border border-neutral-700">{post.body}</p>
-          {post.image && (
-            <img 
-              src={`data:image/jpeg;base64,${bufferToBase64(post.image)}`} 
-              alt={post.title} 
-              className="max-w-full h-auto mx-auto mt-5" 
-            />
-          )}
-        </div>
-        <UploadForm />
+        </main>
       </Suspense>
     </main>
-  );
+  )
 }
